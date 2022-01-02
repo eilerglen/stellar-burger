e2bbdd7b2620c9827f996b07fd8382f002d96cbe
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-01-02 12:29:25 +0300

    install Dnd

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,14 +2,18 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import styles from './app.module.css';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
 export default function App() {
   return (
     <>
       <AppHeader />
       <div className={styles.wrapper}>
-        <BurgerIngredients />
-        <BurgerConstructor />
+        <DndProvider backend = {HTML5Backend}>
+          <BurgerIngredients />
+          <BurgerConstructor />
+        </DndProvider>  
       </div>
       
     </>
--- src/services/reducers/constructor.jsx
+++ src/services/reducers/constructor.jsx
@@ -2,18 +2,24 @@ import {
   ADD_BUN_BURGER,
   ADD_INGREDIENT_BURGER,
   REMOVE_INGREDIENT_BURGER,
+  REMOVE_ALL_INGREDIENTS_BURGER,
 } from '../actions/constructor'
 
 const initialState = {
-  items: [],
-  bun: null,
-  quantity: [],
+  sortedCart: {
+    bun: {},
+    fillers: [],
+  },
+  counts: {},
 }
 
 export const burgerIngredientsReducer = (state = initialState, action) => {
   switch(action.type) {
-     case  ADD_BUN_BURGER: {
+     case  ADD_INGREDIENT_BURGER: {
        return {...state, bun: action.vun}
+     }
+     case REMOVE_ALL_INGREDIENTS_BURGER: {
+       return initialState
      }
   }
 
